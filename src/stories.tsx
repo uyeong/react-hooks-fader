e11ba78b16b77cfbf6db93c744861ccf505582ab
U-Yeong Ju <uyeong21c@gmail.com>
2020-03-24 21:40:44 +0900

    add SetDurationDifferently story

--- src/stories.tsx
+++ src/stories.tsx
@@ -1,31 +1,62 @@
 import React, { createElement } from 'react';
+import { action } from '@storybook/addon-actions';
+import { withKnobs, boolean, number, object } from '@storybook/addon-knobs';
 import useFader from './index';
 
 export default {
   title: 'useFader',
+  decorators: [withKnobs],
 };
 
 export const Default = () =>
   createElement(() => {
     const [ref, show, setShow] = useFader<HTMLDivElement>({
-      initialShow: true,
-      enter: true,
-      duration: {
-        show: 1,
-        hide: 1,
-      },
-      onEnter: () => console.log('Enter'),
-      onEntered: () => console.log('Entered'),
-      onExit: () => console.log('Exit'),
-      onExited: () => console.log('Exited'),
+      initialShow: boolean('initialShow', true),
+      duration: number('duration', 0.3),
+      onEnter: action('enter'),
+      onEntered: action('entered'),
+      onExit: action('exit'),
+      onExited: action('exited'),
     });
-    function handle() {
+    function handleClick() {
       setShow(!show);
     }
     return (
-      <div className="wrapper">
-        <div ref={ref}>target</div>
-        <button onClick={handle}>click</button>
+      <div className="demo-1">
+        <div className="demo-1__item">
+          <div ref={ref}>target</div>
+        </div>
+        <button className="demo-1__btn" onClick={handleClick}>
+          Click Me!
+        </button>
+      </div>
+    );
+  });
+
+export const SetDurationDifferently = () =>
+  createElement(() => {
+    const [ref, show, setShow] = useFader<HTMLDivElement>({
+      initialShow: boolean('initialShow', true),
+      duration: object('duration', {
+        show: 0.5,
+        hide: 0.3,
+      }),
+      onEnter: action('enter'),
+      onEntered: action('entered'),
+      onExit: action('exit'),
+      onExited: action('exited'),
+    });
+    function handleClick() {
+      setShow(!show);
+    }
+    return (
+      <div className="demo-1">
+        <div className="demo-1__item">
+          <div ref={ref}>target</div>
+        </div>
+        <button className="demo-1__btn" onClick={handleClick}>
+          Click Me!
+        </button>
       </div>
     );
   });
